5db3fa81d2d6028a35eda450ae1aa171a808eaa2
Viktor Malych <victormalych@gmail.com>
2019-10-17 10:06:54 +0100

    lab7 finished

---
+++ lab6/src/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/hello/")
+def hello():
+    name = request.args.get('name', '')
+    if name == '':
+        return "no param supplied"
+    else:
+        return "Hello %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+    return str(first+second)
+
+@app.route("/account/", methods=['POST', 'GET'])
+def account():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/uploads/upload.png')
+        return "File Uploaded"
+    else:
+        page ='''
+        <html>
+        <body>
+        <form action="" method="post" name="form" enctype="multipart/form-data">
+            <input type="file" name="datafile" />
+            <input type="submit" name="submit" id="submit"/>
+        </form>
+        </body>
+        <html>
+        '''
+
+        return page, 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ lab7/src/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/users/')
+def users():
+    names = ['simon', 'thomas', 'lee', 'jamie', 'sylvester', 'viktor']
+    return render_template('loops.html', names=names)
+
+@app.route('/inherits/')
+def inherits():
+    return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+    return render_template('inherits1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+    return render_template('inherits2.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
